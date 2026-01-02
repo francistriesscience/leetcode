64ff49d8aab0855742e69ccd649da396b98e21f4
Francis <hello@francistries.science>
2026-01-02 09:34:42 +0800

    feat: Add solution in JavaScript for LeetCode 961 N-Repeated Element in Size 2N Array

---
+++ 961-n-repeated-element-in-size-2n-array/solution.js
@@ -0,0 +1,10 @@
+var repeatedNTimes = function(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return num;
+        }
+        seen.add(num);
+    }
+    return -1;
+};
