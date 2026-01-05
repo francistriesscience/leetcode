f23876c2e38e9b012d1af94c2807b2e71befb221
Francis <hello@francistries.science>
2026-01-05 21:49:45 +0800

    feat: Add solution in Go for LeetCode 1975 Maximum Matrix Sum

---
+++ 1975-maximum-matrix-sum/solution.go
@@ -0,0 +1,30 @@
+func maxMatrixSum(matrix [][]int) int64 {
+	var totalSum int64 = 0
+	minAbsVal := int64(math.MaxInt64)
+	negativeCount := 0
+
+	for _, row := range matrix {
+		for _, val := range row {
+			absVal := int64(val)
+			if absVal < 0 {
+				absVal = -absVal
+			}
+
+			totalSum += absVal
+
+			if val < 0 {
+				negativeCount++
+			}
+
+			if absVal < minAbsVal {
+				minAbsVal = absVal
+			}
+		}
+	}
+
+	if negativeCount%2 == 0 {
+		return totalSum
+	}
+
+	return totalSum - 2*minAbsVal
+}
